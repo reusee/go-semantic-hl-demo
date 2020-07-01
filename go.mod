c48da3787a72b83fa6e92a8de374afa5da7522cb
reus <reusee@gmail.com>
2020-07-01 11:47:06 +0800

    implement

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/reusee/go-semantic-hl-demo
+
+go 1.15
+
+require (
+	github.com/reusee/e/v2 v2.3.3
+	golang.org/x/tools v0.0.0-20200701000337-a32c0cb1d5b2
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"go/ast"
+	"go/token"
+	"os"
+	"time"
+
+	"golang.org/x/tools/go/packages"
+)
+
+func main() {
+	t0 := time.Now()
+	fset := new(token.FileSet)
+	pkgs, err := packages.Load(
+		&packages.Config{
+			Mode: packages.NeedSyntax |
+				packages.NeedTypes |
+				packages.NeedTypesInfo,
+			Fset: fset,
+		},
+		os.Args[1:]...,
+	)
+	ce(err)
+	if packages.PrintErrors(pkgs) > 0 {
+		return
+	}
+	pt("loaded in %v\n", time.Since(t0))
+
+	pkg := pkgs[0]
+	for _, file := range pkg.Syntax {
+		ast.Inspect(file, func(node ast.Node) bool {
+			if node == nil {
+				return true
+			}
+			begin := fset.Position(node.Pos())
+			end := fset.Position(node.End())
+			pt("%v\n\tto %v\n", begin, end)
+			pt("\tsyntax %T\n", node)
+			if expr, ok := node.(ast.Expr); ok {
+				typeAndValue := pkg.TypesInfo.Types[expr]
+				if typeAndValue.Type != nil {
+					pt("\ttype %v\n", typeAndValue.Type)
+				}
+				if typeAndValue.Value != nil {
+					pt("\tvalue %v\n", typeAndValue.Value)
+				}
+			}
+			return true
+		})
+	}
+
+}
